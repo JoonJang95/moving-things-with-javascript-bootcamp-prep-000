591e1fbafe15113794157ba78f0c854d37c78e2c
Joon Jang <joon.jang95@gmail.com>
2018-09-11 02:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var dodger = document.getElementById('dodger')
 
 dodger.style.backgroundColor = '#FF69B4'
 
-
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 
 document.addEventListener('keydown', function(e) {
   
